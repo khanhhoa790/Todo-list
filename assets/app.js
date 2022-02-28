f341fc3c8d6f78c3ee090280a44eae9cc220c5ce
khanhhoa790 <68863404+khanhhoa790@users.noreply.github.com>
2022-02-28 15:16:58 +0700

    fix

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,10 @@
-function Todo(title, description = "", status = "not done") {
+function Task(title, description = "", status = "not done") {
   this.title = title;
   this.description = description;
   this.status = status;
 }
 
-Todo.prototype = {
+Task.prototype = {
   checkDone: function (status) {
     if (status === "done") {
       return "checked";
@@ -14,32 +14,42 @@ Todo.prototype = {
   },
 };
 
-let todo1 = new Todo("Chart", "complete chart", "done");
+let todo1 = new Task("Chart", "complete chart", "done");
 
 console.log(todo1.checkDone(todo1.status));
 
-let TodoList = [
-  new Todo(
-    "Table",
-    "Usce id sagittis sem, a porttitor eros Vivamus eget tincidunt leo. Nula ultricies ligula sed dui porttitor bibendum",
-    "done"
-  ),
-
-  new Todo(
-    "Chart",
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum beatae fugit quaerat iste blanditiis libero dicta eius vel reiciendis! Adipisci saepe cumque itaque vel mollitia labore ipsam officiis illum minima.",
-    "not done"
-  ),
-
-  new Todo(
-    "Shelf",
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum beatae fugit quaerat iste blanditiis",
-    "done"
-  ),
-
-  new Todo("Cereals", "Use id abc", "not done"),
+let taskData = [
+  {
+    title: "Table",
+    description:
+      "Usce id sagittis sem, a porttitor eros Vivamus eget tincidunt leo. Nula ultricies ligula sed dui porttitor bibendum",
+    status: "done",
+  },
+  {
+    title: "Chart",
+    description:
+      "Usce id sagittis sem, a porttitor eros Vivamus eget tincidunt leo. Nula ultricies ligula sed dui porttitor bibendum",
+    status: "not done",
+  },
+  {
+    title: "Shelf",
+    description:
+      "Usce id sagittis sem, a porttitor eros Vivamus eget tincidunt leo. Nula ultricies ligula sed dui porttitor bibendum",
+    status: " not done",
+  },
+  {
+    title: "Cereals",
+    description:
+      "Usce id sagittis sem, a porttitor eros Vivamus eget tincidunt leo. Nula ultricies ligula sed dui porttitor bibendum",
+    status: "done",
+  },
 ];
 
+let TaskList = [];
+
+taskData.forEach((data) => {
+  TaskList.push(new Task(data.title, data.description, data.status));
+});
 const taskTitle = document.getElementById("new-task");
 const taskDescription = document.getElementById("new-description");
 const addButton = document.getElementsByTagName("button")[0];
@@ -47,18 +57,18 @@ const incompleteTaskHolder = document.getElementById("incomplete-tasks");
 const completedTasksHolder = document.getElementById("completed-tasks");
 
 addButton.addEventListener("click", () => {
-  let todoTittle = taskTitle.value;
-  let todoDes = taskDescription.value;
-  if (todoTittle == "") {
+  let taskTittle = taskTitle.value;
+  let taskDes = taskDescription.value;
+  if (taskTittle == "") {
     alert("You need add some thing");
   } else {
-    TodoList.push(new Todo(todoTittle, todoDes));
-    displayTasks(TodoList);
+    TaskList.push(new Task(taskTittle, taskDes));
+    displayTasks(TaskList);
   }
 });
 
-const displayTasks = (TodoList) => {
-  const htmlString = TodoList.map((todo) => {
+const displayTasks = (TaskList) => {
+  const htmlString = TaskList.map((todo) => {
     return `
           <li>
               <input type="checkbox" ${todo.checkDone(todo.status)} />
@@ -71,9 +81,10 @@ const displayTasks = (TodoList) => {
   incompleteTaskHolder.innerHTML = htmlString;
 };
 
-displayTasks(TodoList);
+displayTasks(TaskList);
 
 let listCheckBox = document.querySelectorAll('input[type="checkbox"]');
+console.log(listTask);
 
 // listCheckBox.forEach((inputCheck) => {
 //   let todoTitle = document.querySelectorAll("task-title");
@@ -82,7 +93,7 @@ let listCheckBox = document.querySelectorAll('input[type="checkbox"]');
 //   console.log(todoTitle);
 //   console.log(todoDes);
 //   if (inputCheck.checked == true) {
-//     todoTitle.classList.add("task-complete");
-//     todoDes.classList.add("task-complete");
+//     todoTitle.classList.toggle("task-complete");
+//     todoDes.classList.toggle("task-complete");
 //   }
 // });
